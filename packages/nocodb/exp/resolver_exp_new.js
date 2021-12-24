14712e9df392e46e00a7a73a734234ed91371986
Pranav C <pranavxc@gmail.com>
2021-12-24 19:27:55 +0530

    wip: resolver experiment

--- packages/nocodb/exp/resolver_exp_new.js
+++ packages/nocodb/exp/resolver_exp_new.js
@@ -1,15 +1,67 @@
-const delay = t => new Promise(res => setTimeout(() => res(), t))
-
-const time = 1000;
+const {expect} = require('chai');
+require('mocha');
 const knex = require('knex')({
   client: 'mysql2',
-  connection: {
-    user: 'root',
-    password: 'password',
+  connection: { "user": "root",
+    "password": "password",
+    "port":"3306",
+    "host":"localhost",
     database: 'sakila'
   }
 });
+const delay = t => new Promise(res => setTimeout(() => res(), t))
 
+const simpleInOut = require('./testData/01_simple')
+const compareObj = require('./compareObj')
+const Dataloader = require("dataloader");
+
+const groupBy = (arr, field) => {
+  return (arr || []).reduce((obj, o) => {
+    obj[o[field]] = obj[o[field]] || []
+    obj[o[field]].push(o)
+    return obj;
+  }, {})
+}
+
+
+const cityListLoader = new Dataloader(async function (ids) {
+  const cities = await knex('city').select('*').whereIn('country_id', ids);
+  const gbObj = groupBy(cities, 'country_id')
+  return ids.map(id => (gbObj[id] || []).map(c => new City(c)))
+})
+
+
+const addressCountLoader = new Dataloader(async function (ids) {
+  const cities = await knex('address').select('city_id').count('address_id as count')
+    .groupBy('city_id')
+    .whereIn('city_id', ids);
+  const gbObj = groupBy(cities, 'city_id')
+  return ids.map(id => (gbObj[id] && gbObj[id][0] && gbObj[id][0].count))
+})
+const cityCountLoader = new Dataloader(async function (ids) {
+  const cities = await knex('city').select('country').count('city_id as count')
+    .groupBy('country_id')
+    .whereIn('country_id', ids);
+  const gbObj = groupBy(cities, 'country_id')
+  return ids.map(id => (gbObj[id] && gbObj[id][0] && gbObj[id][0].count))
+})
+
+const addressListLoader = new Dataloader(async function (ids) {
+  const addresses = await knex('address').select('*').whereIn('city_id', ids);
+  const gbObj = groupBy(addresses, 'city_id')
+  return ids.map(id => (gbObj[id] || []).map(c => new Address(c)))
+})
+
+const cityLoader = new Dataloader(async function (ids) {
+  const cities = await knex('city').select('*').whereIn('city_id', ids);
+  const gbObj = groupBy(cities, 'city_id')
+  return ids.map(id => gbObj[id] && gbObj[id][0] ? new City(gbObj[id][0]) : null)
+})
+const countryLoader = new Dataloader(async function (ids) {
+  const countries = await knex('country').select('*').whereIn('country_id', ids);
+  const gbObj = groupBy(countries, 'country_id')
+  return ids.map(id => gbObj[id] && gbObj[id][0] ? new Country(gbObj[id][0]) : null)
+})
 
 class Country {
   constructor(data) {
@@ -17,34 +69,13 @@ class Country {
   }
 
   async cityList() {
-    return (await knex('city').select('*').where({
-      country_id: this.country_id
-    }).limit(1)).map(c => new City(c))
+    return await cityListLoader.load(this.country_id)
   }
 
   async cityCount() {
-    return (await knex('city').count('city_id as count').where({
-      country_id: this.country_id
-    }).first()).count
+    return cityCountLoader.load(this.country_id)
   }
 
-  // async addressCount(args) {
-  //
-  //   return await Promise.all(args.map(c => c.addressCount()))
-  //
-  //   // return (await knex('city').count('city_id as count').where({
-  //   //   country_id: this.country_id
-  //   // }).first()).count
-  // }
-
-  // async addressList(args) {
-  //
-  //   return await Promise.all((await this.cityList()).map(c => c.addressList()))
-  //
-  //   // return (await knex('city').count('city_id as count').where({
-  //   //   country_id: this.country_id
-  //   // }).first()).count
-  // }
 }
 
 
@@ -54,9 +85,7 @@ class Address {
   }
 
   async City() {
-    return new City(await knex('city').select('*').where({
-      city_id: this.city_id
-    }).first())
+    return cityLoader.load(this.city_id)
   }
 
 
@@ -68,45 +97,19 @@ class City {
   }
 
   async Country() {
-    return new Country((await knex('country').select('*').where({
-      country_id: this.country_id
-    }).first()))
+    return countryLoader.load(this.country_id)
   }
 
-  /*  async addressList() {
-      return (await knex('address').select('*').where({
-        city_id: this.city_id
-      })).map(c => new Address(c))
-    }*/
+  async addressList() {
+    return addressListLoader.load(this.city_id)
+  }
 
-    async addressCount() {
-      return (await knex('address').count('city_id as count').where({
-        city_id: this.city_id
-      }).first()).count
-    }
+  async addressCount() {
+    return addressCountLoader.load(this.city_id)
+  }
 
 
 }
-
-
-// const nestResolver = {
-//   country: `INDIA`,
-//   async cityCount() {
-//     await delay(time)
-//     return `12-${time}`
-//   },
-//   async cityList() {
-//     await delay(time)
-//     return {
-//       city: `city 1-${time}`,
-//       async addressCount() {
-//         await delay(time)
-//         return `2-${time}`
-//       },
-//     }
-//   }
-// };
-
 
 const rootAst = {
   Country: {
@@ -126,14 +129,14 @@ const rootAst = {
         type: 'array',
         elementType: 'City',
         // nested: {
-        //   level: 1, dependsOn: ['country_id']
+        //   level: 1, path: ['country_id']
         // }
       },
       cityCount: {
         name: 'cityCount',
         type: 'number',
         // nested: {
-        //   level: 1, dependsOn: ['country_id']
+        //   level: 1, path: ['country_id']
         // }
       },
 
@@ -141,7 +144,15 @@ const rootAst = {
       addressCount: {
         name: 'addressCount',
         type: 'number',
-        nested: {level: 2, dependsOn: ['cityList', 'addressCount']}
+        nested: {level: 2, path: ['cityList', 'addressCount']}
+      },
+      addressList: {
+        type: 'number',
+        nested: {level: 2, path: ['cityList', 'addressList', 'address']}
+      },
+      d: {
+        type: 'number',
+        nested: {level: 2, path: ['cityList', 'addressList', 'City', 'Country']}
       }
     }
   }, City: {
@@ -160,7 +171,7 @@ const rootAst = {
       }, addressList: {
         name: 'addressList',
         type: 'array',
-        elementType: 'City'
+        elementType: 'Address'
       }, countryRead: {
         name: 'countryRead',
         type: 'Country'
@@ -172,7 +183,7 @@ const rootAst = {
         name: 'Country',
         type: 'Country',
         // nested: {
-        //   level: 1, dependsOn: ['country_id']
+        //   level: 1, path: ['country_id']
         // }
       },
     }
@@ -189,15 +200,15 @@ const rootAst = {
         type: 'City',
         // nested: {
         //   level: 1,
-        //   dependsOn: ['city_id']
+        //   path: ['city_id']
         // }
       },
-      Country: {
+      CountryName: {
         name: 'Country',
         type: 'Country',
         nested: {
           level: 1,
-          dependsOn: ['City', 'Country']
+          path: ['City', 'Country', 'country']
         }
       }
     }
@@ -219,7 +230,7 @@ const nestResolver = {
   },
 
   async CountryList() {
-    return (await knex('country').limit(1)).map(c => new Country(c))
+    return (await knex('country').limit(2)).map(c => new Country(c))
   },
 
   async AddressList() {
@@ -227,166 +238,19 @@ const nestResolver = {
   }
 }
 
-
-/*
-
-const reqExecutor = async (reqObj, resObj, _ast) => {
-
-  const res = {}
-  // const dependedFields = Object.keys(reqObj).map(k => (ast.fields && ast.fields[k] && ast.fields[k].nested && ast.fields[k].nested.dependsOn))
-  /!*  const dependFields = new Set();
-    for(const k of Object.keys(reqObj)){
-      if(ast.fields && ast.fields[k] && ast.fields[k].nested && ast.fields[k].nested.dependsOn){
-        dependFields.add(ast.fields[k].nested.dependsOn)
-
-      }
-    }*!/
-
-  function extractDependsOn(key, i = 0, prev, __ast) {
-    const ast = __ast || _ast
-    if (!prev || typeof prev !== 'object' || i >= _ast.fields[key].nested.dependsOn.length) {
-      if (typeof resObj[key] === 'function') {
-        return resObj[key](prev)//.call(res);
-        // console.log(prefix + res[key])
-      } else if (typeof resObj[key] === 'object') {
-        return Promise.resolve(resObj[key])
-        // console.log(prefix + res[key])
-      } else {
-        return Promise.resolve(resObj[key])
-        // console.log(prefix + res[key])
-      }
-    }
-    if (typeof prev[key] === 'function') {
-      return resObj[key](res)//.call(res);
-      // console.log(prefix + res[key])
-    }
-
-
-    if (!prev[_ast.fields[key].nested.dependsOn[i]])
-      extractField(_ast.fields[key].nested.dependsOn[i], _ast)
-    prev[key] = prev[_ast.fields[key].nested.dependsOn[i]].then(next => {
-      // return extractDependsOn(key, ++i, next)
-
-
-      if (Array.isArray(next)) {
-        return Promise.all(next.map(r => extractDependsOn(key, ++i, r, rootAst[_ast[ast.fields[key].nested.dependsOn[i]].elementType])))
-      } else {
-        return extractDependsOn(reqObj[key], ++i, next, _ast[ast.fields[key].nested.dependsOn[i]])
-      }
-
-    })
-    return prev[key]
-  }
-
-
-  function extractField(key, __ast) {
-    const ast = __ast || _ast
-
-    if (!(ast.fields && ast.fields[key] && ast.fields[key].nested)) {
-      if (typeof resObj[key] === 'function') {
-        res[key] = resObj[key]()//.call(res);
-        // console.log(prefix + res[key])
-      } else if (typeof resObj[key] === 'object') {
-        res[key] = Promise.resolve(resObj[key])
-        // console.log(prefix + res[key])
-      } else {
-        res[key] = Promise.resolve(resObj[key])
-        // console.log(prefix + res[key])
-      }
-    } else {
-      extractDependsOn(key, 0, res)
-    }
-
-
-  }
-
-
-  for (const key of Object.keys(reqObj)) {
-    if (key in resObj) {
-      extractField(key);
-    }
-
-
-    if (reqObj[key] && typeof reqObj[key] === 'object') {
-      res[key] = res[key].then(res1 => {
-        if (Array.isArray(res1)) {
-          return Promise.all(res1.map(r => reqExecutor(reqObj[key], r, rootAst[_ast[key].elementType])))
-        } else {
-          return reqExecutor(reqObj[key], res1, _ast[key])
-        }
-      })
-    }
-
-
-  }
-
-  await Promise.all(Object.values(res))
-
-  const out = {};
-  for (const [k, v] of Object.entries(res)) {
-    out[k] = await v
-  }
-
-
-  return out
-}
-*/
-
-/*
-const reqExecutor = async (reqObj, resObj, ast) => {
-
-  const res = {}
-
-
-  await Promise.all(Object.keys(reqObj).map(async (key) => {
-    if (key in resObj && !(ast.fields && ast.fields[key] && ast.fields[key].dependsOn)) {
-
-      if (typeof resObj[key] === 'function') {
-        res[key] = await resObj[key]()//.call(res);
-        // console.log(prefix + res[key])
-      } else if (typeof resObj[key] === 'object') {
-        res[key] = resObj[key]
-        // console.log(prefix + res[key])
-      } else {
-        res[key] = resObj[key]
-        // console.log(prefix + res[key])
-      }
-      await delay(100)
-    }
-    if (reqObj[key] && typeof reqObj[key] === 'object') {
-      if (Array.isArray(res[key])) {
-        res[key] = await Promise.all(res[key].map(r => reqExecutor(reqObj[key], r, rootAst[ast[key].elementType])))
-      } else {
-        res[key] = await reqExecutor(reqObj[key], res[key], ast[key])
-      }
-    }
-
-  }))
-
-  return res
+const flattenArray = (res) => {
+  return Array.isArray(res) ? res.flatMap(r => flattenArray(r)) : res
 }
 
-*/
 
+const execute = async (requestObj, resolverObj, ast, objTree = {}) => {
 
-const execute = async (requestObj, resolverObj, ast) => {
-
-  const res = {}
-  // const dependedFields = Object.keys(reqObj).map(k => (ast.fields && ast.fields[k] && ast.fields[k].nested && ast.fields[k].nested.dependsOn))
-  /*  const dependFields = new Set();
-    for(const k of Object.keys(reqObj)){
-      if(ast.fields && ast.fields[k] && ast.fields[k].nested && ast.fields[k].nested.dependsOn){
-        dependFields.add(ast.fields[k].nested.dependsOn)
-
-      }
-    }*/
-
-
+  const res = []
+  // extract nested(lookup) recursively
   const extractNested = (path, o = {}, resolver) => {
     if (path.length) {
-      const key =path[0]
+      const key = path[0]
       if (!o[key]) {
-
 
 
         if (typeof resolver[key] === 'function') {
@@ -401,13 +265,12 @@ const execute = async (requestObj, resolverObj, ast) => {
         }
 
 
-      }else if(typeof o[key] === 'function'){
+      } else if (typeof o[key] === 'function') {
         o[key] = o[key]()
       }
 
 
-
-      return (o[path[0]] instanceof  Promise ? o[path[0]] : Promise.resolve(o[path[0]])).then(res1 => {
+      return (o[path[0]] instanceof Promise ? o[path[0]] : Promise.resolve(o[path[0]])).then(res1 => {
 
         if (Array.isArray(res1)) {
           return Promise.all(res1.map(r => extractNested(path.slice(1), r)))
@@ -422,37 +285,31 @@ const execute = async (requestObj, resolverObj, ast) => {
   };
 
 
+  // function for extracting field
   function extractField(key) {
     if (!(ast && ast && ast[key] && ast[key].nested)) {
-      if (typeof resolverObj[key] === 'function') {
-        res[key] = resolverObj[key]()//.call(res);
-        // console.log(prefix + res[key])
-      } else if (typeof resolverObj[key] === 'object') {
-        res[key] = Promise.resolve(resolverObj[key])
-        // console.log(prefix + res[key])
-      } else {
-        res[key] = Promise.resolve(resolverObj[key])
-        // console.log(prefix + res[key])
-      }
-    } else {
-/*      if (!res[ast[key].nested])
-        extractField(ast[key].nested.dependsOn)
-
-
-      res[key] = res[ast.fields[key].nested.dependsOn].then(res => {
+      if (resolverObj) {
+        // resolve if it's resolver function
         if (typeof resolverObj[key] === 'function') {
-          return resolverObj[key](res)//.call(res);
+          res[key] = resolverObj[key]()//.call(res);
           // console.log(prefix + res[key])
         } else if (typeof resolverObj[key] === 'object') {
-          return Promise.resolve(resolverObj[key])
+          res[key] = Promise.resolve(resolverObj[key])
           // console.log(prefix + res[key])
         } else {
-          return Promise.resolve(resolverObj[key])
+          res[key] = Promise.resolve(resolverObj[key])
           // console.log(prefix + res[key])
         }
-      })*/
-      res[key] = extractNested(ast[key].nested.dependsOn, res, resolverObj)
+      }
 
+      objTree[key] = res[key]
+
+    } else {
+
+      // if nested extract the nested value
+      res[key] = extractNested(ast[key].nested.path, objTree, resolverObj).then(res => {
+        return Promise.resolve(flattenArray(res))
+      })
 
 
     }
@@ -462,17 +319,14 @@ const execute = async (requestObj, resolverObj, ast) => {
 
 
   for (const key of Object.keys(requestObj)) {
-    // if (key in resolverObj) {
-      extractField(key);
-    // }
-
+    extractField(key);
 
     if (requestObj[key] && typeof requestObj[key] === 'object') {
       res[key] = res[key].then(res1 => {
         if (Array.isArray(res1)) {
-          return Promise.all(res1.map(r => execute(requestObj[key], r, rootAst[ast[key].elementType] && rootAst[ast[key].elementType].fields)))
+          return Promise.all(res1.map(r => execute(requestObj[key], r, rootAst[ast[key].elementType] && rootAst[ast[key].elementType].fields, objTree[key] = {})))
         } else {
-          return execute(requestObj[key], res1, ast[key].fields)
+          return execute(requestObj[key], res1, ast[key].fields, objTree[key] = {})
         }
       })
     }
@@ -484,68 +338,26 @@ const execute = async (requestObj, resolverObj, ast) => {
 
   const out = {};
   for (const [k, v] of Object.entries(res)) {
-    out[k] = await v
+    if (k in requestObj)
+      out[k] = await v
   }
 
 
   return out
 }
 
-
-const req = {
-  // AddressList: {
-  //   City: {
-  //     Country: 1
-  //   },
-  //   Country: 1
-  // },
-  CountryList:{
-    // cityList:{
-    //   addressCount:1
-    // },
-    addressCount:1
+describe('Resolver', async () => {
+  for (const {name, in: input, out} of simpleInOut) {
+    it(`Resolver : ${name}`, async function () {
+      this.timeout(50000)
+      console.time(name)
+      const res = await execute(input, nestResolver, rootAst);
+      console.timeEnd(name)
+      console.log(JSON.stringify(res, 0, 2))
+      expect(compareObj(out, res)).to.be.eq(true)
+    });
   }
-};
-
-
-const extractNested = (path, o = {}) => {
-  if (path.length) {
-    if (!o[path[0]]) {
-      o[path[0]] = Promise.resolve({})
-    }
-    return o[path[0]].then(r => extractNested(path.slice(1), r))
-  } else {
-    return Promise.resolve(o)
-  }
-};
-
-(async () => {
-
-  console.time('start')
-  console.log(JSON.stringify(await execute(req, nestResolver, rootAst), 0, 2));
-  console.timeEnd('start')
-
-
-
-/*  console.log(JSON.stringify(await extractNested(['a', 'b'], {
-    a: Promise.resolve({
-      b: Promise.resolve({
-        t: 1
-      })
-    })
-  }), 0, 2))*/
-
-
-/*  const o = {};*/
-
-/*  console.log(JSON.stringify(await extractNested(['a', 'b'], o), 0, 2))
-  console.log(o)
-  console.log(JSON.stringify(await extractNested(['a', 'c'], o), 0, 2))
-  console.log(JSON.stringify(await extractNested(['a', 'b', 'c'], o), 0, 2))
-  console.log(JSON.stringify(await extractNested(['a', 'b'], o), 0, 2))*/
-
-})().catch(e => console.log(e)).finally(() => process.exit(0))
-
+})
 
 
 
