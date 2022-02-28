0a6bc050f09cfb37ce60669e0195a6980bcdccbd
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-02-28 21:01:25 +0800

    wip: add getList & setList
    Signed-off-by: Wing-Kam Wong <wingkwong.code@gmail.com>

--- packages/nocodb/src/lib/noco-cache/CacheMgr.ts
+++ packages/nocodb/src/lib/noco-cache/CacheMgr.ts
@@ -1,9 +1,15 @@
 export default abstract class CacheMgr {
-  public abstract get(key: string): Promise<any>;
+  public abstract get(key: string, type: number): Promise<any>;
   public abstract set(key: string, value: any, ttl?: any): Promise<any>;
   public abstract clear(): Promise<any>;
   public abstract del(key: string): Promise<any>;
   public abstract getAll(pattern: string): Promise<any[]>;
   public abstract delAll(pattern: string): Promise<any[]>;
   public abstract getOne(pattern: string): Promise<any>;
+  public abstract getList(scope: string, list: string[]): Promise<any[]>;
+  public abstract setList(
+    scope: string,
+    subListKeys: string[],
+    list: any[]
+  ): Promise<boolean>;
 }
