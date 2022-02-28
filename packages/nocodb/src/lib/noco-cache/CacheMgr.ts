5c023ca0522ddd69c7dffb519eee4072de5c191a
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-02-28 15:50:29 +0800

    chore: revise parameters
    Signed-off-by: Wing-Kam Wong <wingkwong.code@gmail.com>

--- packages/nocodb/src/lib/noco-cache/CacheMgr.ts
+++ packages/nocodb/src/lib/noco-cache/CacheMgr.ts
@@ -1,6 +1,6 @@
 export default abstract class CacheMgr {
-  public abstract get(key: string, config?: any): Promise<any>;
-  public abstract set(key: string, value: any, config?: any): Promise<any>;
+  public abstract get(key: string): Promise<any>;
+  public abstract set(key: string, value: any, ttl?: any): Promise<any>;
   public abstract clear(): Promise<any>;
   public abstract del(key: string): Promise<any>;
   public abstract getAll(pattern: string): Promise<any[]>;
