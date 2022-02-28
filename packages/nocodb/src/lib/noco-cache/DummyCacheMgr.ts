bd3ee421d4ee35fc3a8300e1706b0651b092181a
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-02-28 21:02:05 +0800

    chore: disable unused mgr
    Signed-off-by: Wing-Kam Wong <wingkwong.code@gmail.com>

--- packages/nocodb/src/lib/noco-cache/DummyCacheMgr.ts
+++ packages/nocodb/src/lib/noco-cache/DummyCacheMgr.ts
@@ -1,40 +1,48 @@
-import CacheMgr from './CacheMgr';
-
-export default class DummyCacheMgr extends CacheMgr {
-  clear(): Promise<any> {
-    return Promise.resolve(undefined);
-  }
-
-  del(_key: string): Promise<any> {
-    return Promise.resolve(undefined);
-  }
-
-  delAll(_pattern: string): Promise<any[]> {
-    return Promise.resolve([]);
-  }
-
-  get(_key: string, _config?: any): Promise<any> {
-    return Promise.resolve(undefined);
-  }
-
-  getAll(_pattern: string): Promise<any[]> {
-    return Promise.resolve([]);
-  }
-
-  getOne(_pattern: string): Promise<any> {
-    return Promise.resolve(undefined);
-  }
-
-  set(_key: string, _value: any, _config?: any): Promise<any> {
-    return Promise.resolve(undefined);
-  }
-
-  setv2(
-    _key: string,
-    _secondaryKey: string,
-    _value: any,
-    _config?: any
-  ): Promise<any> {
-    return Promise.resolve(undefined);
-  }
-}
+// import CacheMgr from './CacheMgr';
+//
+// export default class DummyCacheMgr extends CacheMgr {
+//   clear(): Promise<any> {
+//     return Promise.resolve(undefined);
+//   }
+//
+//   del(_key: string): Promise<any> {
+//     return Promise.resolve(undefined);
+//   }
+//
+//   delAll(_pattern: string): Promise<any[]> {
+//     return Promise.resolve([]);
+//   }
+//
+//   get(_key: string, _config?: any): Promise<any> {
+//     return Promise.resolve(undefined);
+//   }
+//
+//   getAll(_pattern: string): Promise<any[]> {
+//     return Promise.resolve([]);
+//   }
+//
+//   getOne(_pattern: string): Promise<any> {
+//     return Promise.resolve(undefined);
+//   }
+//
+//   set(_key: string, _value: any, _config?: any): Promise<any> {
+//     return Promise.resolve(undefined);
+//   }
+//
+//   async getList(): Promise<string[]> {
+//     return Promise.all([]);
+//   }
+//
+//   async setList(): Promise<boolean> {
+//     return Promise.resolve(undefined);
+//   }
+//
+//   // setv2(
+//   //   _key: string,
+//   //   _secondaryKey: string,
+//   //   _value: any,
+//   //   _config?: any
+//   // ): Promise<any> {
+//   //   return Promise.resolve(undefined);
+//   // }
+// }
