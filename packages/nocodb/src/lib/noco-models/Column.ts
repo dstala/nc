11b4ec56de3ebe9e9df3aeca40a89561c6242894
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-03-05 14:59:45 +0800

    fix: missing columns
    Signed-off-by: Wing-Kam Wong <wingkwong.code@gmail.com>

--- packages/nocodb/src/lib/noco-models/Column.ts
+++ packages/nocodb/src/lib/noco-models/Column.ts
@@ -127,7 +127,9 @@ export default class Column implements ColumnType {
       ncMeta
     );
 
-    return row;
+    return await this.get({
+      colId: row.id
+    });
   }
 
   private static async insertColOption<T>(
