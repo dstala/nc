10d8e6ee163bcee333192c468dca7710cfab85c7
Pranav C <pranavxc@gmail.com>
2022-03-12 00:37:15 +0530

    fix: correction in util function
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nocodb/src/lib/noco/meta/api/helpers/extractProps.ts
+++ packages/nocodb/src/lib/noco/meta/api/helpers/extractProps.ts
@@ -1,6 +1,6 @@
 export default function extractProps(body: any, props: string[]) {
   return props.reduce((o, key) => {
     if (key in body) o[key] = body[key];
-    return body;
+    return o;
   }, {});
 }
